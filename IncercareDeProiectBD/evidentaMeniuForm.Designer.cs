e8a99681976f161f58adfa275456d826a69d4278
Adrian Leventiu <adilev2003@yahoo.com>
2023-01-06 22:45:15 +0200

    New functionalities

--- IncercareDeProiectBD/evidentaMeniuForm.Designer.cs
+++ IncercareDeProiectBD/evidentaMeniuForm.Designer.cs
@@ -28,20 +28,284 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonCloseApp = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonCloseApp
+            // 
+            this.buttonCloseApp.AccessibleDescription = "buttonCloseApp";
+            this.buttonCloseApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCloseApp.Location = new System.Drawing.Point(670, 392);
+            this.buttonCloseApp.Name = "buttonCloseApp";
+            this.buttonCloseApp.Size = new System.Drawing.Size(118, 46);
+            this.buttonCloseApp.TabIndex = 11;
+            this.buttonCloseApp.Text = "Inchide Aplicatia";
+            this.buttonCloseApp.UseVisualStyleBackColor = true;
+            this.buttonCloseApp.Click += new System.EventHandler(this.buttonCloseApp_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 144);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(670, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 40);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Insereaza";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Stergere dupa ID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(670, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 40);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Cautare dupa ID";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(670, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 40);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Ingrediente necesare prepararii dupa ID";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(546, 393);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 46);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Actualizeaza afisajul";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(670, 346);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 40);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Modificare produs";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "ID Produs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Nume Produs";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Pret productie";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 300);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
+            this.textBox3.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "ID Categorie Produs";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(118, 263);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 20);
+            this.textBox4.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Pret vanzare";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(118, 339);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(144, 20);
+            this.textBox5.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "ID Bucatar";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 380);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(144, 20);
+            this.textBox6.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(280, 268);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 13);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "1 - Vegetarian; 2 - Vegan; 3 - Uzual";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(280, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(186, 13);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Doar in cazul inserarii unui nou produs";
             // 
             // evidentaMeniuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonCloseApp);
             this.Name = "evidentaMeniuForm";
             this.Text = "evidentaMeniuForm";
             this.Load += new System.EventHandler(this.evidentaMeniuForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCloseApp;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- IncercareDeProiectBD/evidentaMeniuForm.cs
+++ IncercareDeProiectBD/evidentaMeniuForm.cs
@@ -7,19 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
 
 namespace IncercareDeProiectBD
 {
     public partial class evidentaMeniuForm : Form
     {
+
+        OracleConnection c = Class1.GetDBConnection();
+
         public evidentaMeniuForm()
         {
             InitializeComponent();
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "SELECT P.IDPRODUS, P.NUME AS NUMEPRODUS, P.PRETDEPRODUCTIE || ' LEI' AS PRETDEPRODUCTIE, P.PRETDEVANZARE || ' LEI' AS PRETDEVANZARE, CP.NUME AS CATEGORIEPRODUS, R.IDRETETA, B.NUME || ' ' || B.PRENUME AS NUMEBUCATAR, B.POST FROM PRODUS P JOIN CATEGORIEPRODUS CP ON CP.IDCATEGORIEPRODUS = P.IDCATEGORIEPRODUS JOIN RETETA R ON R.IDRETETA = P.IDRETETA JOIN BUCATAR B ON B.IDBUCATAR = R.IDBUCATAR ORDER BY IDPRODUS";
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            c.Close();
         }
 
         private void evidentaMeniuForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonCloseApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "INSERT INTO RETETA(IDRETETA, IDBUCATAR, NUME) VALUES (RETETA_IDRETETA_SEQ.nextval, :IDBUCATAR, :NUME)";
+            cmd.Parameters.Add("IDBUCATAR", textBox6.Text);
+            cmd.Parameters.Add("NUME", textBox2.Text);
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            OracleCommand cmd2 = new OracleCommand();
+            cmd2.Connection = c;
+            cmd2.CommandText = "INSERT INTO PRODUS(IDPRODUS, IDCATEGORIEPRODUS, IDRETETA, NUME, PRETDEPRODUCTIE, PRETDEVANZARE) VALUES (PRODUS_IDPRODUS_SEQ.nextval, :IDCATEGORIEPRODUS, RETETA_IDRETETA_SEQ.currval, :NUME, :PRETDEPRODUCTIE, :PRETDEVANZARE)";
+            cmd2.Parameters.Add("IDCATEGORIEPRODUS", textBox4.Text);
+            cmd2.Parameters.Add("NUME", textBox2.Text);
+            cmd2.Parameters.Add("PRETDEPRODUCTIE", textBox3.Text);
+            cmd2.Parameters.Add("PRETDEVANZARE", textBox5.Text);
+            OracleDataReader dr2 = cmd2.ExecuteReader();
+
+
+            c.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "DELETE FROM PRODUS WHERE IDPRODUS = :IDPRODUS";
+            cmd.Parameters.Add("IDPRODUS", textBox1.Text);
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            c.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "SELECT P.IDPRODUS, P.NUME AS NUMEPRODUS, P.PRETDEPRODUCTIE || ' LEI' AS PRETDEPRODUCTIE, P.PRETDEVANZARE || ' LEI' AS PRETDEVANZARE, CP.NUME AS CATEGORIEPRODUS, R.IDRETETA, B.NUME || ' ' || B.PRENUME AS NUMEBUCATAR, B.POST FROM PRODUS P JOIN CATEGORIEPRODUS CP ON CP.IDCATEGORIEPRODUS = P.IDCATEGORIEPRODUS JOIN RETETA R ON R.IDRETETA = P.IDRETETA JOIN BUCATAR B ON B.IDBUCATAR = R.IDBUCATAR ORDER BY IDPRODUS";
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            c.Close();
         }
     }
 }
