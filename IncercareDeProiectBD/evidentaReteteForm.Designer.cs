2107c953ec9816ace95446c143282e7b89cb0b19
Adrian Leventiu <adilev2003@yahoo.com>
2023-01-07 23:41:21 +0200

    qu

--- IncercareDeProiectBD/evidentaReteteForm.Designer.cs
+++ IncercareDeProiectBD/evidentaReteteForm.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.buttonCloseApp = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 144);
+            this.dataGridView1.TabIndex = 53;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(670, 340);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 46);
+            this.button5.TabIndex = 59;
+            this.button5.Text = "Actualizeaza afisajul";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buttonCloseApp
+            // 
+            this.buttonCloseApp.AccessibleDescription = "buttonCloseApp";
+            this.buttonCloseApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCloseApp.Location = new System.Drawing.Point(670, 392);
+            this.buttonCloseApp.Name = "buttonCloseApp";
+            this.buttonCloseApp.Size = new System.Drawing.Size(118, 46);
+            this.buttonCloseApp.TabIndex = 58;
+            this.buttonCloseApp.Text = "Inchide Aplicatia";
+            this.buttonCloseApp.UseVisualStyleBackColor = true;
+            this.buttonCloseApp.Click += new System.EventHandler(this.buttonCloseApp_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(425, 236);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 40);
+            this.button6.TabIndex = 82;
+            this.button6.Text = "Modificare reteta";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 40);
+            this.button2.TabIndex = 81;
+            this.button2.Text = "Cautare dupa ID";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 40);
+            this.button1.TabIndex = 80;
+            this.button1.Text = "Stergere dupa ID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 40);
+            this.button3.TabIndex = 79;
+            this.button3.Text = "Insereaza reteta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 89;
+            this.label7.Text = "Nume Reteta";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(103, 256);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(144, 20);
+            this.textBox6.TabIndex = 88;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 224);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 87;
+            this.label8.Text = "ID Bucatar";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(103, 221);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(144, 20);
+            this.textBox7.TabIndex = 86;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(36, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 85;
+            this.label9.Text = "ID Reteta";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(103, 184);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(144, 20);
+            this.textBox8.TabIndex = 84;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 294);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(652, 144);
+            this.dataGridView2.TabIndex = 90;
+            // 
+            // evidentaReteteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.buttonCloseApp);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "evidentaReteteForm";
             this.Text = "evidentaReteteForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button buttonCloseApp;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- IncercareDeProiectBD/evidentaReteteForm.cs
+++ IncercareDeProiectBD/evidentaReteteForm.cs
@@ -7,14 +7,151 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
 
 namespace IncercareDeProiectBD
-{
+{ 
     public partial class evidentaReteteForm : Form
     {
+
+        OracleConnection c = Class1.GetDBConnection();
+
         public evidentaReteteForm()
         {
             InitializeComponent();
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "SELECT * FROM RETETA ORDER BY IDRETETA";
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            c.Close();
+        }
+        public void hideForm()
+        {
+            Hide();
+        }
+
+        private void buttonCloseApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "SELECT * FROM RETETA";
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            c.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "INSERT INTO RETETA(IDRETETA, IDBUCATAR, NUME) VALUES (INGREDIENT_IDINGREDIENT_SEQ.nextval, :IDBUCATAR, :NUME)";
+            cmd.Parameters.Add("IDBUCATAR", textBox7.Text);
+            cmd.Parameters.Add("NUME", textBox6.Text);
+            OracleDataReader dr = cmd.ExecuteReader();
+
+
+            OracleCommand cmd2 = new OracleCommand();
+            cmd2.Connection = c;
+            cmd2.CommandText = "SELECT * FROM RETETA WHERE IDRETETA = (SELECT MAX(IDRETETA) FROM RETETA)";
+            OracleDataReader dr2 = cmd2.ExecuteReader();
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd2);
+            DataTable dt2 = new DataTable();
+            da.Fill(dt2);
+            dataGridView2.DataSource = dt2;
+
+            MessageBox.Show("Vei fi redirectionat la pagina de organizare retete pentru a finaliza reteta!");
+
+            var newForm = new evidentaOrganizareRetete();
+            hideForm();
+            newForm.Show();
+
+            c.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "SELECT * FROM RETETA WHERE IDRETETA = :IDRETETA";
+            cmd.Parameters.Add("IDRETETA", textBox8.Text);
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            OracleCommand cmd2 = new OracleCommand();
+            cmd2.Connection = c;
+            cmd2.CommandText = "SELECT DR.IDDETALIURETETA, DR.IDRETETA, DR.IDINGREDIENT, I.NUME AS INGREDIENT, DR.CANTITATE || ' ' || I.UNITATEMASURA AS CANTITATENECESARA FROM DETALIURETETA DR JOIN INGREDIENT I ON I.IDINGREDIENT = DR.IDINGREDIENT WHERE DR.IDRETETA = :IDRETETA ORDER BY I.IDINGREDIENT";
+            cmd2.Parameters.Add("IDRETETA", textBox8.Text);
+            OracleDataReader dr2 = cmd2.ExecuteReader();
+
+            OracleDataAdapter da2 = new OracleDataAdapter(cmd2);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            dataGridView2.DataSource = dt2;
+
+            c.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "DELETE FROM RETETA WHERE IDRETETA = :IDRETETA";
+            cmd.Parameters.Add("IDRETETA", textBox8.Text);
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            c.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            c.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = c;
+            cmd.CommandText = "UPDATE RETETA SET IDBUCATAR = :IDBUCATAR, NUME = :NUME WHERE IDRETETA = :IDRETETA";
+            cmd.Parameters.Add("IDBUCATAR", textBox7.Text);
+            cmd.Parameters.Add("NUME", textBox6.Text);
+            cmd.Parameters.Add("IDRETETA", textBox8.Text);
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            MessageBox.Show("Vei fi redirectionat la pagina de organizare retete pentru a finaliza reteta!");
+
+            var newForm = new evidentaOrganizareRetete();
+            hideForm();
+            newForm.Show();
+
+            c.Close();
         }
     }
 }
